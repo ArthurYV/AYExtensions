d9900593475aef9c3fbb84337d71e5a7fd6a20ec
ArthurYV <yangjingbing007@gmail.com>
2016-08-28 09:54:44 +0800

    add ArraySlice+Extension

---
+++ AYExtensions/ArraySlice+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  ArraySlice+Extension.swift
+//  AYExtensions
+//
+//  Created by Arthury on 8/28/16.
+//  Copyright © 2016 Arthury. All rights reserved.
+//
+
+#if os(OSX)
+    import AppKit
+#else
+    import UIKit
+#endif
+
+
