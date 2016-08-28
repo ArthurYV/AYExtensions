c4c79e726c1b67c3f4ecd4922010cd0f500fc8e6
ArthurYV <yangjingbing007@gmail.com>
2016-08-28 09:35:33 +0800

    add Swift+Extension

---
+++ AYExtensions/AYExtensions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AYExtensions
+//
+//  Created by Arthury on 8/28/16.
+//  Copyright © 2016 Arthury. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ AYExtensions/String+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  String+Extension.swift
+//  AYExtensions
+//
+//  Created by Arthury on 8/28/16.
+//  Copyright © 2016 Arthury. All rights reserved.
+//
+
+#if os(OSX)
+    import AppKit
+#else
+    import UIKit
+#endif
+
+extension String {
+    var slice: ArraySlice<Character> {
+        return ArraySlice(self.characters)
+    }
+}
